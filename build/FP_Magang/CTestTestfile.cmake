7fe9579f1ab5ad7bb5d7adb23db63888bbd2ff17
Gilbran Mahda <gbrnmhd89@gmail.com>
2024-11-02 01:17:37 +0700

    Initial commit

---
+++ build/FP_Magang/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/gilbran/MagangIRIS/MagangIRIS-FP/src/FP_Magang
+# Build directory: /home/gilbran/MagangIRIS/MagangIRIS-FP/build/FP_Magang
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
