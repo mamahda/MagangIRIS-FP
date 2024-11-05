95de2b4906ece8dcb98b5899be253f702fe80c3c
Gilbran Mahda <gbrnmhd89@gmail.com>
2024-11-06 01:10:59 +0700

    update

--- src/FP_Magang/src/robotprocess.cpp
+++ src/FP_Magang/src/robotprocess.cpp
@@ -7,45 +7,182 @@
 #include "FP_Magang/coordinate.h"
 #include "FP_Magang/BS2PC.h"
 
+#define PI 3.141592654
+#define RAD 0.0174532925199 // PI/180
+#define DEG 57.295779513082 // 180/PI
+
 ros::Publisher pub;
 ros::Subscriber stat_sub;
+ros::Subscriber th_sub;
 ros::Subscriber coord_sub;
+ros::Timer init;
+ros::Timer loop;
 FP_Magang::PC2BS cmd;
 
-void statusCallback(const FP_Magang::BS2PC msg) {
-    ROS_INFO("Received status: %.0f", msg.status);
-    ROS_INFO("Target coordinates: x=%.2f, y=%.2f", msg.tujuan_x, msg.tujuan_y);
-}
+int basestatus;
+int resstats, prevstats, stats, xtu, ytu;
+double motor1 = 0, motor2 = 0, motor3 = 0;
+double angle1 = 0, angle2 = 120, angle3 = 240;
+double angle1Rad = angle1 * RAD;
+double angle2Rad = angle2 * RAD;
+double angle3Rad = angle3 * RAD;
+double bx = 0, by = 0, vx = 0, vy = 0, vth = 0, th;
+char keyb;
 
-void coordinateCallback(const FP_Magang::coordinate msg) {
-    ROS_INFO("Received coordinates: x=%.2f, y=%.2f, z=%.2f", msg.x, msg.y, msg.z);
-}
-
-void robot(double motor1, double motor2, double motor3) {
-    cmd.motor1 = motor1;
-    cmd.motor2 = motor2;
-    cmd.motor3 = motor3;
-    pub.publish(cmd);
-    ros::Duration(1).sleep();
-    cmd.motor1 = 0;
-    cmd.motor2 = 0;
-    cmd.motor3 = 0;
-    pub.publish(cmd);
-}
-
-int getch() {
-    static struct termios oldt, newt;
-    tcgetattr(STDIN_FILENO, &oldt);
+void inkey() {
+    static struct termios oldt;
+    static struct termios newt;
+    tcgetattr(STDIN_FILENO, &oldt);           // Simpan pengaturan lama
     newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);
-    newt.c_cc[VMIN] = 0;
-    newt.c_cc[VTIME] = 0;
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    newt.c_lflag &= ~(ICANON);                 // Non-blocking input
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);  // Terapkan pengaturan baru
+    keyb = getchar();  // Baca karakter (non-blocking)
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);  // Kembalikan pengaturan lama
+}
 
-    int c = getchar();
+void inversekinematic(double vx, double vy, double vth) {
+    motor1 = vx * cos(angle1Rad) + vy * sin(angle1Rad) + vth;
+    motor2 = vx * cos(angle2Rad) + vy * sin(angle2Rad) + vth;
+    motor3 = vx * cos(angle3Rad) + vy * sin(angle3Rad) + vth;
+    ROS_INFO("motor1=%.2f motor2=%.2f motor3=%.2f", motor1, motor2, motor3);
 
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-    return c;
+}
+
+void robotspeed(double motor_input1, double motor_input2, double motor_input3) {
+    vx = (2.0 / 3.0) * (motor_input1 * cos(angle1Rad) + motor_input2 * cos(angle2Rad) + motor_input3 * cos(angle3Rad));
+    vy = (2.0 / 3.0) * (motor_input1 * sin(angle1Rad) + motor_input2 * sin(angle2Rad) + motor_input3 * sin(angle3Rad));
+    vth = (motor_input1 + motor_input2 + motor_input3) / 3.0;
+    double tempvx = vx * cos(th) - vy * sin(th);
+    double tempvy = vx * cos(th) + vy * sin(th);
+    vx = tempvx;
+    vy = tempvy;
+
+    inversekinematic(vx, vy, vth);
+    ROS_INFO("vx=%.2f vy=%.2f vth=%.2f", vx, vy, vth);
+}
+
+void status1() {
+    int speed = 250;
+    inkey();
+
+    switch (keyb) {
+    case 'w': // Move forward
+        robotspeed(0, speed, -speed);
+        break;
+    case 's': // Move backward
+        robotspeed(0, -speed, speed);
+        break;
+    case 'd': // Move right
+        robotspeed(speed, -(speed * 0.5), -(speed * 0.5));
+        break;
+    case 'a': // Move left
+        robotspeed(-speed, (-speed * 0.5), (speed * 0.5));
+        break;
+    case 'q': // Rotate counterclockwise
+        robotspeed(speed, speed, speed);
+        break;
+    case 'e': // Rotate clockwise
+        robotspeed(-speed, -speed, -speed);
+        break;
+    default:
+        robotspeed(0, 0, 0);
+        return;
+    }
+}
+
+void statusCallback(const FP_Magang::BS2PC::ConstPtr& msg) {
+    ROS_INFO("Received status = %d", static_cast<int>(msg->status));
+    if (prevstats != msg->status) {
+        switch (static_cast<int>(msg->status)) {
+        case 1:
+            ROS_INFO("Status 1");
+            basestatus = 1111;
+            break;
+        case 2:
+            ROS_INFO("Status 2");
+            basestatus = 2222;
+            break;
+        case 3:
+            ROS_INFO("Status 3");
+            basestatus = 3333;
+            break;
+        case 4:
+            ROS_INFO("Status 4");
+            basestatus = 4444;
+            break;
+        default:
+            basestatus = 0;
+            break;
+        }
+    }
+    stats = msg->status;
+    prevstats = msg->status;
+    xtu = msg->tujuan_x;
+    ytu = msg->tujuan_y;
+}
+
+void thetaCallback(const FP_Magang::BS2PC::ConstPtr& msg) {
+    th = msg->th;
+    while (th > 180 || th < -180) {
+        if (th > 180) {
+            th = -th + 180;
+        }
+        else if (th < -180) {
+            th = -th - 180;
+        }
+    }
+    th = th * RAD;
+    ROS_INFO("Received theta: %.0f", th * DEG);
+
+}
+
+void coordinateCallback(const FP_Magang::coordinate::ConstPtr& msg) {
+    if (basestatus != 1) {
+        bx = msg->x;
+        by = msg->y;
+    }
+    ROS_INFO("Received coordinates: x=%.2f, y=%.2f", msg->x, msg->y);
+}
+
+void loopCallback(const ros::TimerEvent&) {
+    switch (basestatus) {
+    case 1111:
+        status1();
+        break;
+        // case 2222:
+        //     status2();
+        //     break;
+        // case 3333:
+        //     status3();
+        //     break;
+        // case 4444:
+        //     status4();
+        //     break;
+    default:
+        break;
+    }
+
+    FP_Magang::PC2BS pesan;
+    pesan.motor1 = motor1;
+    pesan.motor2 = motor2;
+    pesan.motor3 = motor3;
+    pesan.bola_x = bx;
+    pesan.bola_y = by;
+    pub.publish(pesan);
+}
+
+void resetCallback(const ros::TimerEvent&) {
+    if (resstats != prevstats) {
+        FP_Magang::PC2BS reset;
+        motor1 = 0;
+        motor2 = 0;
+        motor3 = 0;
+        reset.motor1 = motor1;
+        reset.motor2 = motor2;
+        reset.motor3 = motor3;
+        pub.publish(reset);
+        resstats = prevstats;
+    }
 }
 
 // Main node setup and event loop
@@ -53,33 +190,13 @@ int main(int argc, char** argv) {
     ros::init(argc, argv, "publisher");
     ros::NodeHandle nh;
 
-    pub = nh.advertise<FP_Magang::PC2BS>("/pc2bs", 50);
+    init = nh.createTimer(ros::Duration(0.02), resetCallback);
     stat_sub = nh.subscribe("/bs2pc", 50, statusCallback);
+    th_sub = nh.subscribe("/bs2pc", 50, thetaCallback);
     coord_sub = nh.subscribe("/ball_coordinate", 50, coordinateCallback);
+    loop = nh.createTimer(ros::Duration(0.02), loopCallback);
+    pub = nh.advertise<FP_Magang::PC2BS>("/pc2bs", 50);
 
-    ROS_INFO("Node started and waiting for messages...");
-
-    while (ros::ok()) {
-        robot(0, 500, -500);
-        char key = getch();
-        switch (key) {
-        case 'w': robot(0, 5, -5); break;
-        case 's': robot(0, -5, 5); break;
-        case 'a': robot(-5, 2.5, 2.5); break;
-        case 'd': robot(5, -2.5, -2.5); break;
-        default: break;
-        }
-
-        // Buat pesan dan publikasikan
-        your_package::coordinate msg;
-        msg.x = current_value;
-        msg.y = current_value;
-        pub.publish(msg);
-
-        ROS_INFO("Publishing: x=%.2f, y=%.2f", msg.x, msg.y);
-
-
-        ros::spinOnce();
-    }
+    ros::spin();
     return 0;
 }
